c80f9c6d849683774ee35465b209e92ba65a8b8a
eitai <eitaimeir@gmail.com>
2026-05-18 15:01:04 +0300

    צםרק

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,6 +1,6 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import { VitePWA } from 'vite-plugin-pwa'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import { VitePWA } from 'vite-plugin-pwa';
 
 export default defineConfig(({ mode }) => ({
   plugins: [
@@ -74,4 +74,4 @@ export default defineConfig(({ mode }) => ({
       },
     },
   },
-}))
+}));
